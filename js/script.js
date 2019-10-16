4e51fe9c32bd04ba5511d65522144523728bd72f
Filip Taczkowski <filip.taczkowski@gmail.com>
2019-10-16 17:28:21 +0200

    Install styleline and editorconfig

--- js/script.js
+++ js/script.js
@@ -4,29 +4,29 @@
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
-  
+
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-  
+
     /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement: ', clickedElement);
     clickedElement.classList.add('active');
-  
+
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
-  
+
     /* [DONE] get 'href' attribute from the clicked link */
     const hrefClickedLink = clickedElement.getAttribute('href');
     console.log('hrefClickedLink: ', hrefClickedLink);
-  
+
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const clickedArticle = document.querySelector(hrefClickedLink);
     console.log('clickedArticle: ', clickedArticle);
@@ -77,7 +77,7 @@
 
     const links = document.querySelectorAll('.titles a');
     console.log('links: ', links);
-    
+
     for (let link of links){
       link.addEventListener('click', titleClickHandler);
     }
@@ -85,5 +85,5 @@
   }
 
   generateTitleLinks();
-    
+
 }
